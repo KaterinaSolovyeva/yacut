49b200f4490a9fa4d0aa5d434aeeba72061f3cf0
KaterinaSolovyeva <aka-malta@yandex.ru>
2022-05-05 16:38:46 +0300

    One more validation

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,20 +1,19 @@
-import re
-
 from flask import jsonify, request
+from http import HTTPStatus
 
 from . import app, db
 from .error_handlers import InvalidAPIUsage
+from .forms import is_correct
 from .models import URL_map
 from .views import get_unique_short_id
 
-PATTERN = r'[a-zA-Z0-9]'
 
 
 @app.route('/api/id/<string:short_id>/', methods=['GET'])
 def get_original(short_id):
     map = URL_map.query.filter_by(short=short_id).first()
     if map is None:
-        raise InvalidAPIUsage('Указанный id не найден', 404)
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
     return jsonify(url=map.original), 200
 
 
@@ -30,17 +29,16 @@ def add_short_link():
     else:
         short_id = data['custom_id']
     if len(short_id) > 16:
-        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки', 400)
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки', HTTPStatus.BAD_REQUEST)
     if URL_map.query.filter_by(short=short_id).first() is not None:
         raise InvalidAPIUsage(f'Имя "{short_id}" уже занято.')
-    accord = re.findall(PATTERN, short_id)
-    if ''.join(accord) == short_id:
+    if is_correct(short_id):
         map = URL_map()
         data['original'] = data['url']
         data['short'] = short_id
         map.from_dict(data)
         db.session.add(map)
         db.session.commit()
-        return jsonify(short_link='http://localhost/' + map.short, url=map.original), 201
+        return jsonify(short_link='http://localhost/' + map.short, url=map.original), HTTPStatus.CREATED
     else:
-        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки', 400)
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки', HTTPStatus.BAD_REQUEST)
--- yacut/forms.py
+++ yacut/forms.py
@@ -1,6 +1,17 @@
+import re
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, URLField
+from wtforms import StringField, SubmitField, URLField, ValidationError
 from wtforms.validators import DataRequired, Optional
+from .models import URL_map
+
+
+PATTERN = r'[a-zA-Z0-9]'
+
+
+def is_correct(short_id):
+    accord = re.findall(PATTERN, short_id)
+    return ''.join(accord) == short_id
 
 
 class URL_mapForm(FlaskForm):
@@ -8,8 +19,13 @@ class URL_mapForm(FlaskForm):
         'Длинная ссылка',
         validators=[DataRequired(message='Обязательное поле')]
     )
-    #Length(1, 16) убрано и условие перенесено во views для прохождения теста
     custom_id = StringField(
         'Ваш вариант короткой ссылки', validators=[Optional()]
     )
     submit = SubmitField('Создать')
+
+    def validate_custom_id(form, field):
+        if not is_correct(field.data):
+            raise ValidationError('Указано недопустимое имя для короткой ссылки')
+        if URL_map.query.filter_by(short=field.data).first():
+            raise ValidationError(f'Имя {field.data} уже занято!')
